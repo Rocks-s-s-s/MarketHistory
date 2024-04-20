7313a5dca6cb298d8a1a855375b55401bc395966
Андрей Боев <a.boev.it@gmail.com>
2024-04-20 17:31:23 +0300

    Fix 2.0 Подготовленные запросы вместо replace

--- db_tools.py
+++ db_tools.py
@@ -1,25 +1,18 @@
 def insert_to_db(date, start_lot, end_lot, cnx):
     template = ("INSERT INTO market.good_intervals"
-             "(share_id, timestamp_begin, timestamp_ends)"
-             "VALUES('SBER',to_timestamp('<date1> <time1>','yyyy-mm-dd hh24:mi:ss'),to_timestamp('<date2> <time2>','yyyy-mm-dd hh24:mi:ss'));")
-    s = template.replace('<time1>',str(start_lot))
-    s = s.replace('<time2>', str(end_lot))
-    s = s.replace('<date1>', str(date))
-    s = s.replace('<date2>', str(date))
-    #print(s)
+                "(share_id, timestamp_begin, timestamp_ends)"
+                "VALUES('SBER', to_timestamp('%s %s','yyyy-mm-dd hh24:mi:ss'), to_timestamp('%s %s','yyyy-mm-dd hh24:mi:ss'));")
     cursor = cnx.cursor()
-    cursor.execute(s)
+    cursor.execute(template, (date, start_lot, date, end_lot))
     cnx.commit()
     cursor.close()
 
+
 def get_all_share_id(cnx):
     query = ("select s.share_id "
-    "from market.shares s inner join market.load_info li on s.share_id = li.share_id " 
-    "where li.timestamp_load < current_date-1 "
-    "and s.is_history ='X'")
-    #query = ("select distinct  share_id "
-    #        "FROM market.shares "
-    #        "order by share_id")
+             "from market.shares s inner join market.load_info li on s.share_id = li.share_id "
+             "where li.timestamp_load < current_date-1 "
+             "and s.is_history ='X'")
     cursor = cnx.cursor()
     cursor.execute(query)
     id = cursor.fetchall()
